e7f71ec121046f4d3621cfab4ce1e74323b10e67
Your Name <you@example.com>
2020-01-20 19:03:57 -0500

    Did lab

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/drawColorUnif_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/drawColorUnif_fs4x.glsl
@@ -29,10 +29,12 @@
 //	2) assign uniform variable to output color
 
 out vec4 rtFragColor;
-in uniform color;
+uniform vec4 uColor;
 
 void main()
 {
-
-	rtFragColor = color;
+	// DUMMY OUTPUT: all fragments are OPAQUE WHITE
+	//rtFragColor = vec4(1.0, 1.0, 1.0, 1.0);
+	
+	rtFragColor = uColor;
 }
