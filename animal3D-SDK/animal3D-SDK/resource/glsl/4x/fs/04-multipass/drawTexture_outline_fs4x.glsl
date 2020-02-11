ad21585719a87074e6d06f8c77f2238c76acd65a
David <david.bakaleinik@mymail.champlain.edu>
2020-02-11 15:09:46 -0500

    implementing "Soble" edge detection
    Not really Sobel and not really our code, but it workes and we did change it a little

--- animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/04-multipass/drawTexture_outline_fs4x.glsl
+++ animal3D-SDK/animal3D-SDK/resource/glsl/4x/fs/04-multipass/drawTexture_outline_fs4x.glsl
@@ -30,47 +30,68 @@
 
 //https://gamedev.stackexchange.com/questions/68401/how-can-i-draw-outlines-around-3d-models
 
-uniform sampler2D uImage0;
-uniform sampler2D uImage1;
-uniform sampler2D uImage2;
-uniform sampler2D uImage3;
-uniform sampler2D uImage4;
-uniform sampler2D uImage5;
-uniform sampler2D uImage6;
-uniform sampler2D uImage7;
+																								//original code taken from: https://gist.github.com/Hebali/6ebfc66106459aacee6a9fac029d0115
 
+uniform sampler2D uImage0; //Base/composite texture
+uniform sampler2D uImage1; //Base object shapes (?)
+uniform sampler2D uImage2; 
+uniform sampler2D uImage3; 
+uniform sampler2D uImage4; 
+uniform sampler2D uImage5; 
+uniform sampler2D uImage6;  
+uniform sampler2D uImage7; //Earth map texture
+
+//Output of the previous shader
+uniform sampler2D screenTexture;
+
+//Stuff we already have
 layout (location = 0) out vec4 rtFragColor;
 layout (location = 3) out vec4 texCoord;
 in vec4 coord;
 
+																								// Sobel Edge Detection Filter
+																								// GLSL Fragment Shader
+																								// Original Implementation by Patrick Hebron
+																								// Modified by David Bakaleinik
 
-uniform float 		width;
-uniform float 		height;
-
-void make_kernel(inout vec4 n[9], sampler2D tex, vec2 coord)
+//Function that creates a 3x3 kernel (stored in an array), through sampling of 9 screen regions
+void make_kernel(inout vec4 kernel_array[9], sampler2D image, vec2 coordinate)
 {
-	float w = 1.0 / width;
-	float h = 1.0 / height;
+//Calculating the dimantions of the onput texture
+	float width = 1.0 / textureSize(image, 0).x;
+	float height = 1.0 / textureSize(image, 0).y;
 
-	n[0] = texture2D(tex, coord + vec2( -w, -h));
-	n[1] = texture2D(tex, coord + vec2(0.0, -h));
-	n[2] = texture2D(tex, coord + vec2(  w, -h));
-	n[3] = texture2D(tex, coord + vec2( -w, 0.0));
-	n[4] = texture2D(tex, coord);
-	n[5] = texture2D(tex, coord + vec2(  w, 0.0));
-	n[6] = texture2D(tex, coord + vec2( -w, h));
-	n[7] = texture2D(tex, coord + vec2(0.0, h));
-	n[8] = texture2D(tex, coord + vec2(  w, h));
+	//populating the array through repeated sampling of the given sampler2D texture, offset by the dimentions of the texture
+	kernel_array[0] = texture2D(image, coordinate + vec2( -width, -height));
+	kernel_array[1] = texture2D(image, coordinate + vec2(0.0, -height));
+	kernel_array[2] = texture2D(image, coordinate + vec2(  width, -height));
+	kernel_array[3] = texture2D(image, coordinate + vec2( -width, 0.0));
+	kernel_array[4] = texture2D(image, coordinate);
+	kernel_array[5] = texture2D(image, coordinate + vec2(  width, 0.0));
+	kernel_array[6] = texture2D(image, coordinate + vec2( -width, height));
+	kernel_array[7] = texture2D(image, coordinate + vec2(0.0, height));
+	kernel_array[8] = texture2D(image, coordinate + vec2(  width, height));
 }
 
 void main(void) 
 {
-	vec4 n[9];
-	make_kernel( n, uImage2, coord.xy );
+	vec4 kernel_array[9]; //Array of 9 vec4 - potentially represents a kernel
+	make_kernel( kernel_array, uImage1, coord.xy );
 
-	vec4 sobel_edge_h = n[2] + (2.0*n[5]) + n[8] - (n[0] + (2.0*n[3]) + n[6]);
-  	vec4 sobel_edge_v = n[0] + (2.0*n[1]) + n[2] - (n[6] + (2.0*n[7]) + n[8]);
-	vec4 sobel = sqrt((sobel_edge_h * sobel_edge_h) + (sobel_edge_v * sobel_edge_v));
+	//creating the horizontal and vertical gradient maps
+	vec4 sobel_edge_h = kernel_array[2] + (2.0*kernel_array[5]) + kernel_array[8] - (kernel_array[0] + (2.0*kernel_array[3]) + kernel_array[6]);
+  	vec4 sobel_edge_v = kernel_array[0] + (2.0*kernel_array[1]) + kernel_array[2] - (kernel_array[6] + (2.0*kernel_array[7]) + kernel_array[8]);
 
-	rtFragColor = vec4( 1.0 - sobel.rgb, 1.0 );
+	//combination of the horizonal and vertical gradients to produce the final result 
+	vec4 sobel_final = sqrt((sobel_edge_h * sobel_edge_h) + (sobel_edge_v * sobel_edge_v));
+
+	//getiing the on-screen image
+	vec4 screen_Sample = texture(screenTexture, coord.xy);
+
+	float outlineThickness = 10.0f;
+
+	//final output - sobel produces a black image with edges highlighted in white, so it is reversed by sburtracting sobel.rgb from 1
+	//line thickness can be adjusted using a float
+	//the vec4 is then multiplied by the existing image on the screen to apply the outlines
+	rtFragColor = vec4( 1.0 - sobel_final.rgb * outlineThickness, 1.0 ) * screen_Sample;
 }
