7d686c40867c20ab502765bcb03adaca9a7a519e
David Bakaleinik <david.bakaleinik@mymail.champlain.edu>
2020-04-02 15:57:39 -0400

    Implementing Forward Kinematics

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_a3_demo_utilities/_src/animation/a3_Kinematics.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_a3_demo_utilities/_src/animation/a3_Kinematics.c
@@ -39,13 +39,26 @@ extern inline a3i32 a3kinematicsSolveForwardPartial(const a3_HierarchyState *hie
 	if (hierarchyState && hierarchyState->poseGroup && 
 		firstIndex < hierarchyState->poseGroup->hierarchy->numNodes && nodeCount)
 	{
-	//	a3i32 parentIndex;
+		a3i32 parentIndex;
 		a3ui32 i, end = firstIndex + nodeCount;
 		end = a3minimum(end, hierarchyState->poseGroup->hierarchy->numNodes);
 
 		for (i = firstIndex; i < end; ++i)
 		{
 			// ****TO-DO: implement forward kinematics algorithm
+			parentIndex = hierarchyState->poseGroup->hierarchy->nodes[i].parentIndex;
+			
+			if (parentIndex < 0 )
+			{
+				//Is Root
+				hierarchyState->objectSpace->transform[i] = hierarchyState->localSpace->transform[i];
+			}
+			else
+			{
+				//Not Root
+
+				a3real4x4Product(hierarchyState->objectSpace->transform[i].m, hierarchyState->objectSpace->transform[parentIndex].m, hierarchyState->localSpace->transform[i].m);
+			}
 
 		}
 
